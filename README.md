e4456af290452671d98a67975d80f7a0d65d1e0b
S Amarnath Reddy <amarnathreddy0201@gmail.com>
2023-08-18 09:35:13 +0530

    read file and requirements

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mqtt_mongo_docker_test
 For creating mqtt publisher and broker and storing data using mongodb
+
+# Run the main.py
+    * Run the main.py by using python main.py
--- test/mqtt.py
+++ test/mqtt.py
@@ -8,7 +8,7 @@ MQTT_BROKER = "broker.emqx.io" #"127.0.0.1"
 MQTT_PORT = 1883
 MQTT_KEEPALIVE = 6000
 MQTT_QOS = 2
-MQTT_TOPICS = ("python/mqtt")  # Array of topics to subscribe; '#' subscribe to ALL available topics
+MQTT_TOPICS = ("python/#")  # Array of topics to subscribe; '#' subscribe to ALL available topics
 
 MQTT_BROKER = os.getenv("MQTT_BROKER", MQTT_BROKER)
 MQTT_PORT = os.getenv("MQTT_PORT", MQTT_PORT)
@@ -20,6 +20,7 @@ if isinstance(MQTT_TOPICS, str):
 
 
 class MQTT(object):
+    """Mqtt subscriber"""
     def __init__(self, mongo: Mongo):
         self.mongo: Mongo = mongo
         self.mqtt_client = mqtt.Client()
@@ -35,15 +36,18 @@ class MQTT(object):
 
     # noinspection PyUnusedLocal
     def on_message(self, client: mqtt.Client, userdata, msg: mqtt.MQTTMessage):
+        """Save the data in mongobd on message received."""
         print("Rx MQTT")
         self.mongo.save(msg)
 
     def run(self):
+        """run the mqtt server"""
         print("Running MQTT")
         self.mqtt_client.connect(MQTT_BROKER, MQTT_PORT, MQTT_KEEPALIVE)
         self.mqtt_client.loop_forever()
 
     def stop(self):
+        """For stop the mqtt"""
         print("Stopping MQTT")
         self.mqtt_client.loop_stop()
         self.mqtt_client.disconnect()
